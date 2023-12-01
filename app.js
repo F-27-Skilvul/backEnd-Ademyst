6e5e39fe98e587be37bb585f5e0e50002c4ba73c
Andiagung <ipossible24@gmail.com>
2023-12-01 09:59:32 +0800

    update config

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ testConnection();
 app.use(express.json());
 app.use(allRoutes);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173'); // Update with your actual frontend origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
